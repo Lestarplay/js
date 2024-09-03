b2b305bc80ab1d5e38e9b5d944a17a044aa7af0d
Breno B.franco <162745949+Lestarplay@users.noreply.github.com>
2024-09-02 21:23:24 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,3 @@
+window.alert('Minha primeira mensagem!')
+window.confirm('Parábens, Você conseguiu entrar no site!')
+window.prompt('Qual é o seu nome?')
---
+++ js/script02.js
@@ -0,0 +1,4 @@
+alert("Faculdade flamingo")
+function mudarcor(){
+    document.getElementsByTagName("body")[0].style.backgroundColor="#ff5"
+}
