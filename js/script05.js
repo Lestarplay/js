f8e226801201c328f688627554207c4f035ecc67
Breno B.franco <162745949+Lestarplay@users.noreply.github.com>
2024-09-10 21:18:40 -0300

    Add files via upload

---
+++ js/script05.js
@@ -0,0 +1,9 @@
+for (let hora = 0; hora < 24; hora ++){
+    for (let  minuto =  0;  minuto < 60; minuto ++){
+        for (let segundo = 0; segundo < 60; segundo++){
+             console.log(minuto + " : " + segundo)
+        }
+        
+    }
+
+}
